39a76e59b2e71f241dcfdcb5b18d4991445db5c6
Guido <guidoe030@gmail.com>
2024-05-21 18:46:49 -0300

    actulizacion de pagina login, ademas se agrego modal para la opcion de olvide contraseña

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,41 @@
 function toggleMenu() {
-    var menu = document.getElementById('menu');
-    var toggle = document.getElementById('toggle');
-  
-    if (menu.style.display === 'block') {
-      menu.style.display = 'none';
-    } else {
-      menu.style.display = 'block';
-    }
+  var menu = document.getElementById('menu');
+  var toggle = document.getElementById('toggle');
+
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
   }
-  
+}
+
+// Mostrar Contraseña Inicio sesion
+document
+  .getElementById('togglePassword')
+  .addEventListener('click', function () {
+    var passwordField = document.getElementById('password');
+    var passwordFieldType = passwordField.getAttribute('type');
+    if (passwordFieldType === 'password') {
+      passwordField.setAttribute('type', 'text');
+      this.textContent = 'Ocultar';
+    } else {
+      passwordField.setAttribute('type', 'password');
+      this.textContent = 'Mostrar';
+    }
+  });
+
+//Modal para recuperar contraseña, envia un mensaje al correo
+document
+  .getElementById('forgotPasswordForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+    var email = document.getElementById('forgotEmail').value;
+    var messageElement = document.getElementById('forgotPasswordMessage');
+
+    // Simulación de respuesta del servidor:
+    setTimeout(function () {
+      messageElement.textContent =
+        'Si existe una cuenta asociada a este email, recibirás un enlace para restablecer tu contraseña.';
+      messageElement.classList.add('text-success');
+    }, 1000);
+  });
