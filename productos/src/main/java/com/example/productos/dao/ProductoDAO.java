de504e094e6c7cb2e6ecc95309636c554490a227
Leogabii <122978943+Leogabii@users.noreply.github.com>
2024-06-20 11:46:27 -0300

    Update ProductoDAO.java
    Agregue el CRUD faltante

--- productos/src/main/java/com/example/productos/dao/ProductoDAO.java
+++ productos/src/main/java/com/example/productos/dao/ProductoDAO.java
@@ -1,7 +1,10 @@
 package com.example.productos.dao;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,9 +23,42 @@ public class ProductoDAO {
        //cuando creo producto dao me conecto a la db
        MySQLConnector mysql=new MySQLConnector();
        st=mysql.conectarDB();
-       
     }
 
+    public void addProducto(Producto producto) throws SQLException {
+        String sql = "INSERT INTO productos (title, descripcion, precio, image) VALUES (?, ?, ?, ?)";
+        try (Connection conn = st.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, producto.getTitle());
+            pstmt.setString(2, producto.getDescripcion());
+            pstmt.setDouble(3, producto.getPrecio());
+            pstmt.setString(4, producto.getImage());
+            pstmt.executeUpdate();
+        }
+    }
+    
+    
+     public void updateProducto(Producto producto) throws SQLException {
+        String sql = "UPDATE productos SET title = ?, descripcion = ?, precio = ?, image = ? WHERE id = ?";
+        try (Connection conn = st.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+             pstmt.setString(1, producto.getTitle());
+             pstmt.setString(2, producto.getDescripcion());
+             pstmt.setDouble(3, producto.getPrecio());
+             pstmt.setString(4, producto.getImage());
+             pstmt.setInt(5, producto.getId());
+             pstmt.executeUpdate();
+        }
+    }
+
+    public void deleteProducto(Integer id) throws SQLException {
+        String sql = "DELETE FROM productos WHERE id = ?";
+        try (Connection conn = st.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+             pstmt.setInt(1, id);
+             pstmt.executeUpdate();
+        }
+    }
 
 
     public List<Producto> listarProductos() {
@@ -48,5 +84,8 @@ public class ProductoDAO {
         return null;
     }
     
-           }
-  
+    
+    
+    
+    
+}
